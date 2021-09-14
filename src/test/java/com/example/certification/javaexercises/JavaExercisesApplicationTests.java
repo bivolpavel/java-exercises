de5c27e01af84e5318adeca3d940e31c953f5303
ISS-PF1HT8KT\ITSS <byvolpawel@gmail.com>
2021-09-14 21:05:51 +0300

    Initial commit - project setup.

---
+++ src/test/java/com/example/certification/javaexercises/JavaExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.certification.javaexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
